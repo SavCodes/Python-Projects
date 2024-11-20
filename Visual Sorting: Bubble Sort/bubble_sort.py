b69cf76594b96dbb37cf45bc2bff226cf0767cc6
SavCodes <91900522+SavCodes@users.noreply.github.com>
2024-11-20 17:45:33 -0500

    Update bubble_sort.py
    Added BubbleSort class
    -Added class method to display a randomly generated array

--- Visual Sorting: Bubble Sort/bubble_sort.py
+++ Visual Sorting: Bubble Sort/bubble_sort.py
@@ -1,24 +1,38 @@
 import pygame
+import random
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
+class BubbleSorter:
+    def __init__(self, arraySize):
+        self.screen = pygame.display.set_mode((600, 600))
+        self.arraySize = arraySize
+        self.screenWidth, self.screenHeight = self.screen.get_size()
+        self.rectWidth = self.screenWidth / self.arraySize
+        self.array = [random.randint(1,100) for i in range(self.arraySize)]
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    def displayArray(self):
+        for index, rectHeight in enumerate(self.array):
+            rectangleDimensions = (index*self.rectWidth, self.screenHeight - rectHeight, self.rectWidth, rectHeight)
+            pygame.draw.rect(self.screen, (255,255,255), rectangleDimensions, width=0)
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-  
-    # flip() the display to put your work on screen
-    pygame.display.flip()
+    def sortArray(self):
+        pass
 
-    clock.tick(60)  # limits FPS to 60
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((600, 600))
+    clock = pygame.time.Clock()
+    running = True
+    bubbleSorter = BubbleSorter(100)
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill("purple")
+        bubbleSorter.displayArray()
+        pygame.display.flip()
+        clock.tick(60)  # limits FPS to 60
+    pygame.quit()
 
-pygame.quit()
+
+if __name__ == '__main__':
+    main()
