7f7f24bf9658d9a2e1ef62e51866b5a164d3d8af
SavCodes <91900522+SavCodes@users.noreply.github.com>
2024-11-20 22:12:24 -0500

    Update bubble_sort.py
    Refactored code to improve readability and scalability

--- Visual Sorting: Bubble Sort/bubble_sort.py
+++ Visual Sorting: Bubble Sort/bubble_sort.py
@@ -3,6 +3,12 @@ import random
 import time
 import sys
 
+# TO DO LIST:
+# -Add running statistics display (total number of comparisons, number of swaps, total time to run)
+# -Add title text "Bubble Sort"
+# -Add program logo and caption
+# -Add a round corresponding to the height of the rectangle
+
 class Rectangle:
     def __init__(self, height):
         self.height = height
@@ -14,8 +20,9 @@ class BubbleSorter:
         self.arraySize = arraySize
         self.screenWidth, self.screenHeight = self.screen.get_size()
         self.rectWidth = self.screenWidth / self.arraySize
-        self.array = [Rectangle(random.randint(1,100)) for i in range(self.arraySize)]
+        self.array = [Rectangle(random.randint(1,self.screenHeight)) for i in range(self.arraySize)]
         self.endIndex, self.currentIndex = self.arraySize, 0
+        self.comparisonCount, self.swapCount = 0, 0
 
     def displayArray(self):
         for index, rect in enumerate(self.array):
@@ -26,43 +33,60 @@ class BubbleSorter:
             rectangleDimensions = (index*self.rectWidth, self.screenHeight - rect.height, self.rectWidth, rect.height)
             pygame.draw.rect(self.screen, color, rectangleDimensions, width=0)
 
-    def sortArray(self):
-        self.array[self.currentIndex].isBeingSorted = True
+    def checkDoneSorting(self):
         if self.endIndex == 0:
+            print(f"Comparison: {self.comparisonCount}    ||    Swaps: {self.swapCount}")
             time.sleep(5)
             sys.exit()
-            
-        # Once the full listed is sorted through return to the beginning and decrement the maximum index sorted
+
+    def loopThroughRectangles(self):
         if self.currentIndex >= self.endIndex - 1:
             self.array[self.currentIndex].isBeingSorted = False
             self.endIndex -= 1
             self.currentIndex = 0
 
-        # If the current rectangle height is larger than the next rectangle's height, they swap positions
+    def compareAndSwap(self):
         if self.array[self.currentIndex].height > self.array[self.currentIndex+1].height:
+            self.swapCount += 1
             self.array[self.currentIndex], self.array[self.currentIndex+1] = self.array[self.currentIndex+1], self.array[self.currentIndex]
 
+    def sortArray(self):
+        # Keeps track of statistics related to sorting
+        self.comparisonCount += 1
+        # Update sort state of rectangle to active
+        self.array[self.currentIndex].isBeingSorted = True
+        # Once the full array of rectangles is sorted exit the program
+        self.checkDoneSorting()
+        # Once the rectangle is fully sorted, return to the beginning and decrement the maximum index sorted
+        self.loopThroughRectangles()
+        # If the current rectangle height is larger than the next rectangle's height, they swap positions
+        self.compareAndSwap()
+        # Display the updated rectangle states
         self.displayArray()
+        # Update sort status of rectangle to inactive
         self.array[self.currentIndex].isBeingSorted = False
+        # Increment to next rectangle comparison
         self.currentIndex += 1
 
+    def display_statistics(self):
+        pass
 
-def main():
+def main(rectangleNumber):
     pygame.init()
     screen = pygame.display.set_mode((600, 600))
     clock = pygame.time.Clock()
     running = True
-    bubbleSorter = BubbleSorter(30)
+    bubbleSorter = BubbleSorter(rectangleNumber)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         screen.fill("purple")
         bubbleSorter.sortArray()
+        bubbleSorter.display_statistics()
         pygame.display.flip()
-        clock.tick(60)  # limits FPS to 60
+        clock.tick(300)  # limits FPS to 60
     pygame.quit()
 
-
 if __name__ == '__main__':
-    main()
+    main(rectangleNumber=100)
