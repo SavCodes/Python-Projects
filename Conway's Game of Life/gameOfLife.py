ebd43eb4757b524130bb8570178387ff2b512ca8
SavCodes <91900522+SavCodes@users.noreply.github.com>
2024-11-23 20:58:29 -0500

    Update gameOfLife.py
    Added GameBoard class to track overall game state
    Added Cell class to track individual cell states (ie: alive, dead)

--- Conway's Game of Life/gameOfLife.py
+++ Conway's Game of Life/gameOfLife.py
@@ -1,11 +1,21 @@
 import pygame
 
+class GameBoard:
+    def __init__(self, board_size):
+        self.board_size = board_size
+        self.cell_states = [Cell(i,j) for i in range(self.board_size) for j in range(self.board_size)]
+        print(self.cell_states)
+
 class Cell:
     def __init__(self, x, y):
         self.x_position = x
         self.y_position = y
         self.is_alive = False
-           
+        self.neighbors = list()
+
+    def __repr__(self):
+        return f"Cell({self.x_position}, {self.y_position})"
+
 def event_checker():
     events = pygame.event.get()
     for event in events:
@@ -18,6 +28,7 @@ def initialize_pygame():
     pygame.display.set_caption("Game of Life")
 
 def main():
+    gameBoard: GameBoard = GameBoard(10)
     running = True
     initialize_pygame()
     screen = pygame.display.set_mode((800, 600))
