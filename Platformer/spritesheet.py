d824c0beceda9b7f98becfb42686a33eb7289455
SavCodes <91900522+SavCodes@users.noreply.github.com>
2024-11-26 12:58:33 -0500

    Update spritesheet.py
    Updated to automatically pull number of animations from file name

--- Platformer/spritesheet.py
+++ Platformer/spritesheet.py
@@ -1,10 +1,10 @@
 import pygame
 
 class SpriteSheet():
-    def __init__(self, sprite_sheet, number_of_animations):
+    def __init__(self, sprite_sheet):
         self.sprites = pygame.image.load(sprite_sheet)
+        self.number_of_animations = int(sprite_sheet.split(".")[0][-1])
         self.sprite_group = pygame.sprite.Group()
-        self.number_of_animations = number_of_animations
         self.frame_list = [self.sprites.subsurface((32*index,0, 32, 32)) for index in range(self.number_of_animations)]
 
 
