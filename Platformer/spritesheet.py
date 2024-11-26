6a7b69d928091082ddb15f849d91391290c491ad
SavCodes <91900522+SavCodes@users.noreply.github.com>
2024-11-26 11:09:19 -0500

    Update spritesheet.py
    Changed spritesheet handling to pygame sprite group format

--- Platformer/spritesheet.py
+++ Platformer/spritesheet.py
@@ -1,16 +1,24 @@
 import pygame
 
-class SpriteSheet:
+class SpriteSheet():
     def __init__(self, sprite_sheet):
-        self.sprite = pygame.image.load(sprite_sheet)
-        self.frame_list = []
+        self.sprites = pygame.image.load(sprite_sheet)
+        self.sprite_group = pygame.sprite.Group()
 
-    def get_frames(self, frame, width, height, scale, color):
-        image = pygame.Surface((width,height))
-        image.blit(self.sprite, (0,0), ((frame*width), 0, width, height))
-        image.set_colorkey(color)
-        image = pygame.transform.scale(image, (width*scale, height*scale))
-        return image
+    def get_frames(self, width, height, scale, color):
+        number_of_frames = self.sprites.get_width() // width
+        for frame in range(number_of_frames):
+            image = pygame.Surface((width,height))
+            image.set_colorkey(color)
+            image = pygame.transform.scale(image, (width*scale, height*scale))
+            image = image.blit(self.sprites, (0,0), ((frame*width), 0, width, height))
+            FRAME = Frame(image)
+            self.sprite_group.add(FRAME)
+
+class Frame(pygame.sprite.Sprite):
+    def __init__(self, frame):
+        pygame.sprite.Sprite.__init__(self)
+
 
 
 
