89f381f46d30b61a54f3fae0b9063d0699d8b15c
SavCodes <91900522+SavCodes@users.noreply.github.com>
2024-11-26 00:30:29 -0500

    Update main.py
    Added ability to control player with arrow keys

--- Platformer/main.py
+++ Platformer/main.py
@@ -4,24 +4,54 @@ class Player:
     def __init__(self):
         self.screen_width, self.screen_height = 800, 600
 
+        # Initialize player dimensions
         self.player_height = 100
         self.player_width = 20
 
+        # Initialize player position
         self.x_position = self.screen_width / 2
         self.y_position = self.screen_height - self.player_height
+
+        # Initialize player velocities
+        self.x_velocity = 0
+        self.y_velocity = 0
+
+        # Initialize player accelerations
+        self.x_acceleration = 0
+        self.y_acceleration = 0
+
 
     def display_player(self):
         player_rect = (self.x_position, self.y_position, self.player_width, self.player_height)
         player = pygame.draw.rect(pygame.display.get_surface(), (255, 255, 255), player_rect)
 
+
+    def move_player(self):
+        self.x_position += self.x_velocity
+        self.y_position += self.y_velocity
+        self.x_velocity += self.x_acceleration
+        self.y_velocity += self.y_acceleration
+
+
 def initialize_pygame():
     pygame.init()
 
-def event_checker():
+def event_checker(player):
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
             return False
+        keys = pygame.key.get_pressed()
+
+            # Move player based on pressed keys
+        if keys[pygame.K_LEFT]:
+            player.x_velocity = -0.5
+        if keys[pygame.K_RIGHT]:
+            player.x_velocity = 0.5
+        if keys[pygame.K_UP]:
+            player.y_velocity = -0.5
+        if keys[pygame.K_DOWN]:
+            player.y_velocity = 0.5
     return True
 
 def main():
@@ -29,8 +59,13 @@ def main():
     player_one = Player()
     screen = pygame.display.set_mode((800, 600))
     while running:
-        running = event_checker()
+        screen.fill((0, 0, 0))
+        running = event_checker(player_one)
+
+        #PLAYER LOGIC
+        player_one.move_player()
         player_one.display_player()
+
         pygame.display.update()
 
 if __name__ == '__main__':
