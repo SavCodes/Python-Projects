3e7b5c0bd0ebf912bbc5a8abfbc2569244cbe5e1
SavCodes <91900522+SavCodes@users.noreply.github.com>
2024-11-26 16:07:01 -0500

    Update spritesheet.py
    Added a class parameter tat allows for images to be scaled up or down when loaded in. Default scaling factor is 1

--- Platformer/spritesheet.py
+++ Platformer/spritesheet.py
@@ -1,12 +1,13 @@
 import pygame
 
 class SpriteSheet():
-    def __init__(self, sprite_sheet):
+    def __init__(self, sprite_sheet, scale=1.0):
         self.sprites = pygame.image.load(sprite_sheet)
         self.number_of_animations = int(sprite_sheet.split(".")[0][-1])
         self.sprite_group = pygame.sprite.Group()
+        self.scale = scale
         self.frame_list = [self.sprites.subsurface((32*index,0, 32, 32)) for index in range(self.number_of_animations)]
-
+        self.frame_list = [pygame.transform.scale_by(frame, scale) for frame in self.frame_list]
 
 
 
