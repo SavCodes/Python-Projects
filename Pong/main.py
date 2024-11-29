cd156971d702199681dc6fb11543f4d92eb3c391
SavCodes <91900522+SavCodes@users.noreply.github.com>
2024-11-28 23:19:28 -0500

    added "To Do List"

--- Pong/main.py
+++ Pong/main.py
@@ -2,6 +2,12 @@ import pygame
 import paddle
 import ball
 
+# TO DO LIST:
+# - Add score keeping function and display between rounds
+# - Add round timer and total game timer
+# - Add dynamic bouncing to paddles based on movement or collision location
+# - Add music, program caption, and program icon
+
 # Check if the game was quit
 def event_checker():
     events = pygame.event.get()
@@ -17,14 +23,14 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
     clock = pygame.time.Clock()
-    
+
     # Create the ball
     game_ball = ball.Ball(screen)
-    
+
     # Create player paddles
     player_one = paddle.Paddle(screen.get_width() * 0.05 , screen.get_height() / 2, arrow_key_controls=False)
     player_two = paddle.Paddle(screen.get_width() * 0.95, screen.get_height() / 2)
-    
+
     running = True
     while running:
         screen.fill((0, 0, 0))                              # Reset screen between frames
@@ -34,7 +40,7 @@ def main():
 
         game_ball.check_collisions(player_one, player_two)  # Check if the ball made contact with paddles
         game_ball.render_ball()                             # Display the ball
-        
+
         pygame.display.flip()                               # Update the screen after drawing all objects
         clock.tick(60)                                      # Cap FPS at 60
 
