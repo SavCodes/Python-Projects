cfb8cc81e25e50c5278a728a2731a35d7e722e7b
SavCodes <91900522+SavCodes@users.noreply.github.com>
2024-11-29 19:06:24 -0500

    added score keeping features

--- Pong/ball.py
+++ Pong/ball.py
@@ -6,6 +6,7 @@ class Ball:
         self.screen = screen
         self.game_width = screen.get_width()
         self.game_height = screen.get_height()
+        self.score_font = pygame.font.SysFont("Comic Sans", 24)
 
         # Position related attributes
         self.x_position = self.game_width / 2
@@ -16,6 +17,10 @@ class Ball:
         self.ball_speed = 3
         self.x_velocity = self.ball_speed
         self.y_velocity = self.ball_speed
+
+        # Score related attributes
+        self.left_score = 0
+        self.right_score = 0
 
     def render_ball(self):
         pygame.draw.circle(self.screen, "white", (self.x_position, self.y_position), self.radius)
@@ -38,10 +43,20 @@ class Ball:
         elif self.x_position >= player_two.x_position and player_two.y_position <= self.y_position <= player_two.y_position + player_two.height :
             self.x_velocity *= -1
 
-        elif self.x_position < 0 - 2 * self.radius or self.x_position > self.game_width + 2 * self.radius:
+        # Respawn the ball once off-screen
+        elif self.x_position < 0 - 2 * self.radius or self.x_position > self.game_width + 2 * self.radius :
             self.respawn_ball()
 
     def respawn_ball(self):
         self.x_velocity *= -1
+        self. left_score += 1 if self.x_position > self.game_width else 0
+        self.right_score += 1 if self.x_position < 0 else 0
+
         self.x_position = self.game_width / 2
         self.y_position = self.game_height / 2
+
+    def render_score(self):
+        score_text = self.score_font.render(f'{self.left_score}:{self.right_score}', True, (255,255,255))
+        score_rect = score_text.get_rect(center=(self.screen.width/2, 10))
+        self.screen.blit(score_text, score_rect)
+
