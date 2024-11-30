9aeafc730756f0f120a81ac8993fe82123c959dd
SavCodes <91900522+SavCodes@users.noreply.github.com>
2024-11-30 13:09:17 -0500

    integrated world_generator class

--- Platformer/main.py
+++ Platformer/main.py
@@ -1,7 +1,8 @@
 import pygame
 import player
 import physics
-import game_tile
+import world_generator
+import level_files
 
 GAME_SCALE = 3
 
@@ -24,9 +25,7 @@ def main(game_scale=1):
     player_one = player.Player(scale=game_scale)
 
     # Create test tile set for development
-    y_tile_set = [game_tile.Platform("Tile_01.png", 0, i*32*game_scale, scale=game_scale) for i in range(1,30)] + [game_tile.Platform("Tile_01.png", 960 - 96, i*32*game_scale, scale=game_scale) for i in range(1,10)]
-    x_tile_set = [game_tile.Platform("Tile_01.png", i*32*game_scale, 576 - 96, scale=game_scale) for i in range(1,30)]
-    tile_set = x_tile_set + y_tile_set + [game_tile.Platform("Tile_01.png", 400, 150, scale=game_scale)]
+    tile_set = world_generator.WorldGenerator(level_files.level_one, scale=game_scale).world_tiles
 
     # Create initial pygame objects
     clock = pygame.time.Clock()
@@ -37,14 +36,15 @@ def main(game_scale=1):
         running = event_checker([player_one])                       # Check if game was quit
         screen.blit(background)                                     # Refresh the screen between frames
 
-        player_one.move_player(tile_set, screen)                    # Get movement from input and move player
+        player_one.move_player(tile_set)                            # Get movement from input and move player
         physics.gravity(player_one)                                 # Apply gravity to player object
-        player_one.resolve_collision(tile_set, screen)              # Check for collisions with the world
+        player_one.resolve_collision(tile_set)                      # Check for collisions with the world
         player_one.display_player(screen)                           # Draw the player to the screen
 
         # Other Display
-        for tile in tile_set:
-            tile.draw_platform(screen)
+        for layer in tile_set:
+            for tile in layer:
+                tile.draw_platform(screen)
 
         pygame.display.update()
         clock.tick(30)
