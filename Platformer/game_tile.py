b6c8c52053d2ce2c0d74bc9021c3b94d5bf3dd76
SavCodes <91900522+SavCodes@users.noreply.github.com>
2024-11-30 12:35:06 -0500

    rename game_time.py to game_tile.py and added scaling

--- Platformer/game_tile.py
+++ Platformer/game_tile.py
@@ -1,9 +1,11 @@
 import pygame
 
 class Platform:
-    def __init__(self,tile, x_position, y_position, is_collidable=True, scale=3):
+    def __init__(self,tile, x_position, y_position, is_collidable=True, scale=1):
         self.image = pygame.transform.scale_by(pygame.image.load(tile), scale)
-        self.platform_rect = (x_position, y_position, 32, 32)
+        self.width = 32 * scale
+        self.height = 32 * scale
+        self.platform_rect = pygame.Rect(x_position, y_position, self.width, self.height)
         self.x_position = x_position
         self.y_position = y_position
         self.is_collidable = is_collidable
