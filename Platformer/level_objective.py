0e8df580bf2af15489a692c31dc3a9255239bd9f
SavCodes <91900522+SavCodes@users.noreply.github.com>
2024-12-02 00:03:09 -0500

    added check to make sure both players completed their level before progressing

--- Platformer/level_objective.py
+++ Platformer/level_objective.py
@@ -1,7 +1,6 @@
 import pygame
 import world_generator
 import level_files
-
 from main import GAME_SCALE
 
 class LevelObjective:
@@ -18,10 +17,26 @@ class LevelObjective:
         self.objective_rect = pygame.Rect(self.x_position, self.y_position, self.width, self.height)
         pygame.draw.rect(screen, self.color, self.objective_rect, 3)
 
-    def check_objective_collision(self, player):
-        if player.player_rect.colliderect(self.objective_rect):
-            player.current_level += 1
-            player.tile_set = world_generator.WorldGenerator(level_files.player_one_level_set[player.current_level], scale=GAME_SCALE).world_tiles
-            player.x_position = 300
+    def check_objective_collision(self):
+        if self.player.player_rect.colliderect(self.objective_rect):
+            self.player.level_completed = True
+            self.color = (255,255,255)
 
 
+def check_level_complete(player_one, player_two):
+    if player_one.level_completed and player_two.level_completed:
+
+        # Advance player one to the next level and reset position
+        player_one.current_level += 1
+        print("PLAYER ONE CURRENT LEVEL: ", player_one.current_level)
+        player_one.tile_set = world_generator.WorldGenerator(level_files.player_one_level_set[player_one.current_level], scale=GAME_SCALE).world_tiles
+        print("new player one levels loaded")
+        player_one.level_completed = False
+        player_one.x_position = 300
+
+        # Advance player two to the next level and reset position
+        player_two.current_level += 1
+        print("PLAYER TWO CURRENT LEVEL: ", player_two.current_level)
+        player_two.tile_set = world_generator.WorldGenerator(level_files.player_two_level_set[player_two.current_level], scale=GAME_SCALE).world_tiles
+        player_two.level_completed = False
+        player_two.x_position = 900
