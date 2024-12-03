7919883a929ea578b5d33ab565deba6ba253362b
SavCodes <91900522+SavCodes@users.noreply.github.com>
2024-12-03 15:21:25 -0500

    add optional button color parameter to display method

--- Platformer/button.py
+++ Platformer/button.py
@@ -1,31 +1,48 @@
 import pygame
 
 class Button:
-    def __init__(self, screen, x, y, width, height, color="white", text="testing"):
+    def __init__(self, screen, x, y, width, height, text_color="white", text="testing", font_size=12):
         #================ DIMENSION ATTRIBUTES =====================
         self.x_position = x
         self.y_position = y
         self.width = width
         self.height = height
         self.is_pressed = False
+        self.is_hovering = False
 
         #================ DISPLAY ATTRIBUTES =======================
         self.screen = screen
-        self.color = color
+        self.text_color = text_color
         self.text = text
-        self.font_size = 12
+        self.font_size = font_size
         self.font = pygame.font.SysFont("comicsans", self.font_size)
 
-    def display_button(self):
+    def display_button(self, color=(50,50,50)):
         button_text = self.font.render(self.text, True, "white")
-        button_rect = pygame.Rect(self.x_position, self.y_position, self.width, self.height)
+        button_rect = pygame.Rect(0, 0, self.width, self.height)
+        button_rect.center = (self.x_position, self.y_position)
+        pygame.draw.rect(self.screen, color, button_rect)
         self.screen.blit(button_text, button_rect)
 
     def check_pressed(self, mouse_x, mouse_y):
-        if self.x_position < mouse_x < self.x_position + self.width:
-            if self.y_position < mouse_y < self.y_position + self.height:
+        if self.x_position - self.width / 2 < mouse_x < self.x_position + self.width / 2:
+            if self.y_position - self.height / 2 < mouse_y < self.y_position + self.height / 2:
+                self.display_hover_effect()
                 if pygame.mouse.get_pressed()[0]:
                     self.is_pressed = True
                     print("Pressed: ", self.text)
                 else:
                     self.is_pressed = False
+
+    def display_hover_effect(self):
+        button_rect = pygame.Rect(0, 0, self.width * 1.05, self.height * 1.05)
+        button_rect.center = (self.x_position, self.y_position)
+        pygame.draw.rect(self.screen, (255,255,255), button_rect, int(self.width * 0.05))
+
+
+
+    def display_hover(self):
+        pass
+
+    def set_text(self, text):
+        self.text = text
