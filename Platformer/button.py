44eabe04a5b9dc615f597161e705cd4187744497
SavCodes <91900522+SavCodes@users.noreply.github.com>
2024-12-02 22:20:29 -0500

    added method to see if button is pressed

--- Platformer/button.py
+++ Platformer/button.py
@@ -7,6 +7,7 @@ class Button:
         self.y_position = y
         self.width = width
         self.height = height
+        self.is_pressed = False
 
         #================ DISPLAY ATTRIBUTES =======================
         self.screen = screen
@@ -19,6 +20,12 @@ class Button:
         button_text = self.font.render(self.text, True, "white")
         button_rect = pygame.Rect(self.x_position, self.y_position, self.width, self.height)
         self.screen.blit(button_text, button_rect)
-        
-    def check_pressed(self):
-        pass
+
+    def check_pressed(self, mouse_x, mouse_y):
+        if self.x_position < mouse_x < self.x_position + self.width:
+            if self.y_position < mouse_y < self.y_position + self.height:
+                if pygame.mouse.get_pressed()[0]:
+                    self.is_pressed = True
+                    print("Pressed: ", self.text)
+                else:
+                    self.is_pressed = False
