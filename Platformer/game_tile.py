cd085921bb9e4e27223d22fa192995486ea698cf
SavCodes <91900522+SavCodes@users.noreply.github.com>
2024-12-06 13:52:41 -0500

    reformatted to allow for tile set toggling

--- Platformer/game_tile.py
+++ Platformer/game_tile.py
@@ -4,8 +4,7 @@ class Platform:
     def __init__(self, tile, x_position, y_position, is_collidable=True, scale=1):
         self.tile = tile
         self.tile_number = self.tile[self.tile.rfind("_") + 1:self.tile.rfind(".")]
-        self.image = pygame.transform.scale_by(pygame.image.load(tile), scale)
-        self.image.convert()
+        self.image = pygame.transform.scale_by(pygame.image.load(tile), scale).convert_alpha()
         self.width = 32 * scale
         self.height = 32 * scale
         self.platform_rect = pygame.Rect(x_position, y_position, self.width, self.height)
@@ -15,9 +14,7 @@ class Platform:
         self.collision = None
 
     def __repr__(self):
-        start_index = self.tile.rfind("_") + 1
-        end_index = self.tile.rfind(".")
-        return f'"{self.tile[start_index:end_index]}"'
+        return str(self.tile)
 
     def draw_platform(self, screen):
         screen.blit(self.image, self.platform_rect)
