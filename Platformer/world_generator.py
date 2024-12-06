551c09c936a57ea299581168ffad6aea458d74c5
SavCodes <91900522+SavCodes@users.noreply.github.com>
2024-12-06 13:51:29 -0500

    reformatted to allow for tile set toggling

--- Platformer/world_generator.py
+++ Platformer/world_generator.py
@@ -1,10 +1,9 @@
 import game_tile
 
 class WorldGenerator:
-    def __init__(self, world_array_2d, scale=1, working_directory="./game_assets/tile_files/"):
+    def __init__(self, world_array_2d, scale=1):
         self.world_array = world_array_2d
         self.scale = scale
-        self.working_directory = working_directory
         self.world_tiles = self.create_world_tiles()
 
     def create_world_tiles(self):
@@ -12,9 +11,7 @@ class WorldGenerator:
         for y_index, layer in enumerate(self.world_array):
             _temp_row = []
             for x_index, tile in enumerate(layer):
-                if len(str(tile)) < 2:
-                    tile = f'0{tile}'
-                _temp_row.append(game_tile.Platform(f"{self.working_directory}Tile_{tile}.png", x_index * 32 * self.scale,
-                                    y_index * 32 * self.scale, scale=self.scale, is_collidable=True if tile != "00" else False))
+                _temp_row.append(game_tile.Platform(tile, x_index * 32 * self.scale,
+                                    y_index * 32 * self.scale, scale=self.scale, is_collidable=True if tile.split(".")[-2][-2:] != "00" else False))
             _temp_array.append(_temp_row)
         return _temp_array
