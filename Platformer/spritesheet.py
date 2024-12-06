2122e9c93cd900d97dd9e288e731d14abda1a102
SavCodes <91900522+SavCodes@users.noreply.github.com>
2024-12-06 10:45:19 -0500

    full game backup

--- Platformer/spritesheet.py
+++ Platformer/spritesheet.py
@@ -1,13 +1,16 @@
 import pygame
 
-class SpriteSheet():
-    def __init__(self, sprite_sheet, scale=1.0):
+class SpriteSheet:
+    def __init__(self, sprite_sheet, scale=1.0, width=32, height=32):
+        self.width = width
+        self.height = height
         self.sprites = pygame.image.load(sprite_sheet)
-        self.number_of_animations = int(sprite_sheet.split(".")[0][-1])
+        self.number_of_animations = int(sprite_sheet.split(".")[-2][-1])
         self.sprite_group = pygame.sprite.Group()
         self.scale = scale
-        self.frame_list = [self.sprites.subsurface((32*index,0, 32, 32)) for index in range(self.number_of_animations)]
+        self.frame_list = [self.sprites.subsurface((self.width*index,0, self.width, self.height)) for index in range(self.number_of_animations)]
         self.frame_list = [pygame.transform.scale_by(frame, scale) for frame in self.frame_list]
+
 
 
 
