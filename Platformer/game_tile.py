ed9d823eea2b04577bff67f9ae5f1a7b194a0214
SavCodes <91900522+SavCodes@users.noreply.github.com>
2024-12-06 10:50:28 -0500

    full game backup

--- Platformer/game_tile.py
+++ Platformer/game_tile.py
@@ -1,8 +1,11 @@
 import pygame
 
 class Platform:
-    def __init__(self,tile, x_position, y_position, is_collidable=True, scale=1):
+    def __init__(self, tile, x_position, y_position, is_collidable=True, scale=1):
+        self.tile = tile
+        self.tile_number = self.tile[self.tile.rfind("_") + 1:self.tile.rfind(".")]
         self.image = pygame.transform.scale_by(pygame.image.load(tile), scale)
+        self.image.convert()
         self.width = 32 * scale
         self.height = 32 * scale
         self.platform_rect = pygame.Rect(x_position, y_position, self.width, self.height)
@@ -10,6 +13,11 @@ class Platform:
         self.y_position = y_position
         self.is_collidable = is_collidable
         self.collision = None
+
+    def __repr__(self):
+        start_index = self.tile.rfind("_") + 1
+        end_index = self.tile.rfind(".")
+        return f'"{self.tile[start_index:end_index]}"'
 
     def draw_platform(self, screen):
         screen.blit(self.image, self.platform_rect)
