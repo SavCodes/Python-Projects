3914131ec09afcb557ceafbfa243bf8fcf4c4137
SavCodes <91900522+SavCodes@users.noreply.github.com>
2024-12-06 23:51:19 -0500

    added animation method to class

--- Platformer/spritesheet.py
+++ Platformer/spritesheet.py
@@ -10,6 +10,16 @@ class SpriteSheet:
         self.scale = scale
         self.frame_list = [self.sprites.subsurface((self.width*index,0, self.width, self.height)) for index in range(self.number_of_animations)]
         self.frame_list = [pygame.transform.scale_by(frame, scale) for frame in self.frame_list]
+        self.animation_index = 0
+        self.animation_speed = 0.20
+
+    def basic_animate(self):
+        if self.animation_index < self.number_of_animations - 1:
+            self.animation_index += self.animation_speed
+        else:
+            self.animation_index = 0
+
+        return self.frame_list[int(self.animation_index)]
 
 
 
