cb4a1ff42b7dd235cfaa21739bad93409d523ca2
SavCodes <91900522+SavCodes@users.noreply.github.com>
2024-12-09 09:32:33 -0500

    moved world generation into a function

--- Platformer/world_generator.py
+++ Platformer/world_generator.py
@@ -15,3 +15,9 @@ class WorldGenerator:
                                     y_index * 32 * self.scale, scale=self.scale, is_collidable=True if tile.split(".")[-2][-2:] != "00" else False))
             _temp_array.append(_temp_row)
         return _temp_array
+
+def generate_all_world_layers(full_tile_set, scale=1, current_level=0):
+    background = WorldGenerator(full_tile_set[0][current_level], scale=scale).world_tiles
+    level_tiles = WorldGenerator(full_tile_set[1][current_level], scale=scale).world_tiles
+    foreground = WorldGenerator(full_tile_set[2][current_level], scale=scale).world_tiles
+    return background, level_tiles, foreground
